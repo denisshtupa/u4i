6fa80e6a2a7cddab9b690155eba551483b36fd2a
U4I-denis-shtupa <82824451+U4I-denis-shtupa@users.noreply.github.com>
2021-10-04 14:12:36 +0200

    remove changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import './App.css';
-import React, {useContext} from 'react';
-import { Table } from 'react-bootstrap';
-import { ProductList } from './components/ProductList';
-import { GlobalContext, GlobalProvider } from './context/GlobalState';
-import { Summary } from './components/Summary';
-import { Button } from './components/Button';
+import React from 'react';
 
 function App() {
-  const { products } = useContext(GlobalContext);
   return (
     <div>
-      <GlobalProvider>
-        <div className="jumbotron center main-container">          
-          <h3 className="title">Products List</h3>
-          <Table className="striped bordered hover">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Counts</th>
-                <th>Prices</th>
-              </tr>
-            </thead>
-            <tbody>
-              <ProductList products={products}/>         
-            </tbody>
-          </Table>
-          <Button/>
-          <Summary/>
-        </div>
-      </GlobalProvider>
+      HI
     </div>
   );
 }
